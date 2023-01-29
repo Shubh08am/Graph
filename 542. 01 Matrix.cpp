0460352b81a7acd751fb51951a31d3b041afeb04
Shubham Yadav <shubh08am@gmail.com>
2023-01-29 22:12:10 +0530

    Create 542. 01 Matrix.cpp

---
+++ 542. 01 Matrix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        //bfs approach as we traverse level wise and find min distance
+	    int n = grid.size()  , m = grid[0].size();
+	    vector<vector<int>>vis(n,vector<int>(m,0)),distance(n,vector<int>(m,0));;
+	  //  vector<vector<int>>distance(n,vector<int>(m,0));
+	    queue<pair<pair<int,int>,int>>q;
+	    
+	    for(int i=0;i<n;i++){
+	        for(int j=0;j<m;j++){ 
+ 	            if(grid[i][j]==0){
+	            q.push({{i,j},0});
+	            vis[i][j]=1;
+	            }
+	        } 
+	    }
+	    
+	    vector<int>delRow={-1,0,+1,0};
+	    vector<int>delCol={0,-1,0,+1};
+	    while(!q.empty()){
+	        int row = q.front().first.first;
+	        int col = q.front().first.second;
+	        int step = q.front().second;
+	        q.pop();
+	        //mark its distance 
+	        distance[row][col]=step;
+	        //visit 4 neighbour 
+	        
+	        for(int i=0;i<4;i++){
+	            int nrow = row + delRow[i] ; 
+	            int ncol = col + delCol[i] ; 
+	            
+	            //check valid 
+	     if(nrow>=0 and nrow<n and ncol>=0 and ncol<m and !vis[nrow][ncol] and grid[nrow][ncol]==1){
+	         vis[nrow][ncol]=1;
+	         q.push({{nrow,ncol},step+1});
+	     }
+	        }
+	    }
+	    return distance;
+    }
+};
