3266a2394302dacf33fa425c2500a39517d65b80
Shubham Yadav <shubh08am@gmail.com>
2023-02-01 22:17:03 +0530

    Create Course Schedule.cpp

---
+++ Course Schedule.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>indegree(numCourses,0);
+        vector<int>adjList[numCourses];// adjacenecy list
+        queue<int>q;
+        for(auto it : prerequisites){
+            adjList[it[1]].push_back(it[0]);
+            indegree[it[0]]++;
+        }
+        int topologcalSort=0;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){ 
+                q.push(i);
+            //    cout<<"pushed\n";
+                 }
+        }
+     //   for(auto val : indegree){
+      //      cout<<val<<" ";
+      //  }
+        //push neighbour in queue if indegree[val] becomes 0 
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topologcalSort++;
+            //go and see in adjacenecy list neighbour 
+            for(auto it : adjList[node]){
+            //    cout<<node<< "->" <<it<< "\n";
+                //remove the edge and decrease the indegree 
+                indegree[it]--;
+              //  if indegree[it] becomes 0 push in queue 
+               if(indegree[it]==0){ q.push(it);
+              // cout<<"pushed\n";
+               }
+            }
+        } 
+      //  cout<<"\n"<<topologcalSort<<"\n";
+        return topologcalSort==numCourses ;
+    }
+};
