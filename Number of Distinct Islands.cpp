c748c60466ed29b78c5088ce30095ae84b89a22b
Shubham Yadav <shubh08am@gmail.com>
2023-02-02 18:28:28 +0530

    Update Number of Distinct Islands.cpp

--- Number of Distinct Islands.cpp
+++ Number of Distinct Islands.cpp
@@ -22,6 +22,35 @@ class Solution {
             }
         }
     }
+    
+     void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<int>> &grid, 
+    vector<pair<int, int>> &vp, int baseRow, int baseCol){
+        int n = grid.size() , m = grid[0].size();
+        queue<pair<int, int>> q;
+        vis[row][col] = 1;
+        q.push({row, col});
+        
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+             vp.push_back({row-baseRow,col-baseCol});
+             q.pop();
+            
+           int delRow[]={-1,0,+1,0};
+           int delCol[]={0,-1,0,+1}; 
+           for(int i=0;i<4;i++){
+            int nrow = row+delRow[i];
+            int ncol = col+delCol[i];
+             
+            //check valid 
+        if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !vis[nrow][ncol] && grid[nrow][ncol]==1){
+                vis[nrow][ncol]=1;
+                q.push({nrow,ncol});
+            }
+        }
+    }
+    }
+    
     int countDistinctIslands(vector<vector<int>>& grid) {
  
         vector<pair<int,int>>vp;
@@ -35,7 +64,8 @@ class Solution {
                     vector<pair<int,int>>vp;//stores shape 
                     int baseRow=i;
                     int baseCol=j;
-                    dfs(i,j,vis,grid,vp,baseRow,baseCol);
+                    //dfs(i,j,vis,grid,vp,baseRow,baseCol);
+                    bfs(i,j,vis,grid,vp,baseRow,baseCol);
                     //store unique shape i.e islands in set 
                     s.insert(vp);
                 }
