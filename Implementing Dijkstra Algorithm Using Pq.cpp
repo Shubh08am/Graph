d020ee7428f8654cb244291bc2b652285c84ecb4
Shubham Yadav <shubh08am@gmail.com>
2023-03-04 12:29:52 +0530

    Create Implementing Dijkstra Algorithm Using Pq.cpp

---
+++ Implementing Dijkstra Algorithm Using Pq.cpp
@@ -0,0 +1,33 @@
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+         vector<int>distance(V,1e9);
+         //make min heap 
+         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+         //initially,src with dist = 0 is pushed 
+         pq.push({0,S});
+         //src distance from src is 0 
+         distance[S]=0;
+         
+         while(!pq.empty()){
+             int node = pq.top().second ; 
+             int dist = pq.top().first ; 
+             pq.pop() ; 
+             
+             for(auto it : adj[node]){ 
+                 int edgeWt = it[1] ; 
+                 int adjNode = it[0] ; 
+                 
+                 if(dist+edgeWt < distance[adjNode]){
+                     distance[adjNode] = dist+edgeWt;
+                     pq.push({distance[adjNode] ,adjNode});
+                 }
+             }
+         }
+         return distance;
+    }
+};
